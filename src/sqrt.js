57ce60eccaa09646ff367272ab675e1bafafa164
Ian Macalinao <me@ian.pw>
2015-01-28 23:48:11 -0600

    Sqrt

--- src/sqrt.js
+++ src/sqrt.js
@@ -7,5 +7,23 @@ module.exports = function sqrt(num) {
     }
   }
   return -1;
-}
+};
 
+module.exports.logn = function sqrt(num) {
+  var x = Math.floor(num / 2);
+  var half = x;
+
+  for (;;) {
+    if (x * x == num) {
+      return x;
+    } else if (x * x > num) {
+      if (half == 1) return x - 1;
+      half = Math.floor(half / 2);
+      x -= half;
+    } else {
+      if (half == 1) return x;
+      half = Math.floor(half / 2);
+      x += half;
+    }
+  }
+};
--- test/sqrt.spec.js
+++ test/sqrt.spec.js
@@ -2,10 +2,20 @@ var expect = require('chai').expect;
 var sqrt = require('../src/sqrt');
 
 describe('sqrt', function() {
+
   it('should calculate the correct square root', function() {
     expect(sqrt(4)).to.equal(2);
     expect(sqrt(7)).to.equal(2);
     expect(sqrt(9)).to.equal(3);
     expect(sqrt(15)).to.equal(3);
   });
+
+  it('should calculate the correct square root with logn', function() {
+    var sqrtl = sqrt.logn;
+    expect(sqrtl(4)).to.equal(2);
+    expect(sqrtl(7)).to.equal(2);
+    expect(sqrtl(9)).to.equal(3);
+    expect(sqrtl(15)).to.equal(3);
+  });
+
 });
